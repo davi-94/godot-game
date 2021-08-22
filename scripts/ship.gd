a233ed121dade7f4ac15dae9362ba896b9fd61e1
Davi Pereira <davi.p.m94@gmail.com>
2021-08-21 22:36:14 -0300

    adding shoot

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -5,7 +5,7 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-var speed = 120
+var speed = 130
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +15,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
   var directionX = 0
+  var directionY = 0
 
   if Input.is_action_pressed("ui_left"):
 	  directionX -= 1
@@ -22,5 +23,11 @@ func _process(delta):
   if Input.is_action_pressed("ui_right"):
 	  directionX += 1
 
-  translate(Vector2(directionX, 0) * speed * delta)
+  if Input.is_action_pressed("ui_up"):
+	  directionY -= 1
+	
+  if Input.is_action_pressed("ui_down"):
+	  directionY += 1
+
+  translate(Vector2(directionX, directionY) * speed * delta)
   pass
